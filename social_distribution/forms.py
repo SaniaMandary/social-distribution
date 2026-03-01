47314085fc68584d6ecd892a099c1fc2c009bf61
Ubuntu <rabewar@ualberta.ca>
2026-03-01 07:05:33 -0700

    Modified detail.html to check if text is markdown or plain

--- social_distribution/forms.py
+++ social_distribution/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from .models import TextEntry
 
 class ChangeProfileForm(forms.Form):
     name = forms.CharField(label="name")
@@ -9,3 +10,15 @@ class ChangeProfileForm(forms.Form):
     def clean(self):
         cleaned_data = super().clean()
         return cleaned_data
+
+
+class TextEntryForm(forms.ModelForm):
+    class Meta: 
+        model = TextEntry
+        fields = ['entry_text', 'content_type', 'visibility'] 
+        widgets = {
+            'entry_text': forms.Textarea(attrs={'rows': 5}),
+            'content_type': forms.Select(),
+            'visibility': forms.Select(),
+            }
+            
