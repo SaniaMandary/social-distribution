4cd467e24fdb1c387448327ec646c99daa093c36
Ganesh Saraswat <gsaraswa@ualberta.ca>
2026-03-20 13:52:25 -0600

    fix comment disappeared bug

--- social_distribution/admin.py
+++ social_distribution/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import TextEntry, Author
+from .models import TextEntry, Author, Comment
 
 # Register your models here.
 admin.site.register(TextEntry)
 admin.site.register(Author)
+admin.site.register(Comment)
