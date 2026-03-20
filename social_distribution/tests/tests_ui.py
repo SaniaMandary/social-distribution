a2ba657fb29da5ed30a437dad73e6f69d1c436de
Ganesh Saraswat <gsaraswa@ualberta.ca>
2026-03-20 13:38:56 -0600

    fixed final tests, all tests pass with refactor

--- social_distribution/tests/tests_ui.py
+++ social_distribution/tests/tests_ui.py
@@ -1,10 +1,22 @@
 from django.test import TestCase, Client
 from django.contrib.auth import get_user_model
 from social_distribution.models import Author
+from django.contrib.auth.models import User
+from social_distribution.utils import validate_create_author
 
 # Create your tests here.
 # https://docs.djangoproject.com/en/6.0/topics/testing/overview/
 # https://docs.djangoproject.com/en/6.0/topics/testing/tools/
+
+def make_user_and_author(username="testuser", password="testpass123"):
+    """Create a Django auth user plus a matching Author record."""
+    user = User.objects.create_user(username=username, password=password)
+
+    validate_create_author(username, "localhost")
+    
+    author = Author.objects.get(username=username)
+
+    return user, author
 
 class LoginTest(TestCase):
     def setUp(self):
@@ -48,7 +60,7 @@ class LoginTest(TestCase):
 
         # use wrong password
         response = self.c.post("/social_distribution/api/loginregister/", {"username" : user, "password" : password_wrong}, follow=True)
-        self.assertEqual(response.context['message'], 'UNIQUE constraint failed: auth_user.username')
+        self.assertEqual(response.context['message'], 'Invalid username or password')
         pass
 
 class ChangeProfileTest(TestCase):
@@ -58,13 +70,7 @@ class ChangeProfileTest(TestCase):
         self.user = "test123"
         self.password = "12345"
 
-        get_user_model().objects.create_user(
-            username=self.user,
-            password=self.password
-        )
-        
-        author = Author(url=self.user, name = self.user)
-        author.save()
+        self.user, self.author =make_user_and_author(username=self.user, password=self.password)
 
         self.assertTrue(self.c.login(username=self.user, password=self.password))
         pass
@@ -75,7 +81,7 @@ class ChangeProfileTest(TestCase):
 
     def test_change_profile_context_state(self):
         response = self.c.post("/social_distribution/changeprofile/", follow=True)
-        self.assertEqual(response.context['name'], self.user)
+        self.assertEqual(response.context['name'], self.author.name)
         self.assertEqual(response.context['description'], '')
         self.assertEqual(response.context['picture'], '')
         self.assertEqual(response.context['github'], '')
@@ -88,9 +94,9 @@ class ChangeProfileTest(TestCase):
             'picture': '???.png',
             'github': 'github?'
         }
-        response = self.c.post("/social_distribution/api/editprofile/", data=new_data, follow=True)
-        
-        author = Author.objects.get(pk=self.user)
+        response = self.c.post("/social_distribution/api/editprofile/", data=new_data, follow=True, content_type='application/json')
+
+        author = Author.objects.get(name=new_data['name'])
         self.assertEqual(author.name, new_data["name"])
         self.assertEqual(author.description, new_data["description"])
         self.assertEqual(author.picture, new_data["picture"])
@@ -110,13 +116,7 @@ class IndexProfileTest(TestCase):
         self.user = "test123"
         self.password = "12345"
 
-        get_user_model().objects.create_user(
-            username=self.user,
-            password=self.password
-        )
-        
-        author = Author(url=self.user, name = self.user)
-        author.save()
+        self.user, self.author =make_user_and_author(username=self.user, password=self.password)
 
         self.assertTrue(self.c.login(username=self.user, password=self.password))
         pass
@@ -131,9 +131,9 @@ class IndexProfileTest(TestCase):
     
     def test_add_entry_shows_on_index(self):
         response = self.c.post("/social_distribution/api/addentry/", data={
-            'entry_text':'this is a test'
+            'content':'this is a test'
         }, follow=True)
         
         response = self.c.post("/social_distribution/", follow=True) 
         self.assertEqual(len(response.context['latest_entry_list']), 1)
-        self.assertEqual(response.context['latest_entry_list'][0]['entry_text'], 'this is a test')
+        self.assertEqual(response.context['latest_entry_list'][0].content, 'this is a test')
